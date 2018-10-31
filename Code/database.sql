3d79d35be4185d6da7f3cfec98b81cb4057255ea
jcamacho8199 <37858393+jcamacho8199@users.noreply.github.com>
2018-10-31 18:24:48 -0400

    Update database.sql
    Updated code to capture final merged table structure.

--- Code/database.sql
+++ Code/database.sql
@@ -2,21 +2,35 @@ CREATE DATABASE weather_data;
 USE weather_data;
 CREATE TABLE weather_2016 
 	(date DATE PRIMARY KEY,
-    max_temp INT,
-    min_temp FLOAT,
-    avg_temp FLOAT,
-    precipitation FLOAT,
-    snow_fall FLOAT,
-    snow_depth FLOAT
-    );
+    	 max_temp INT,
+    	 min_temp FLOAT,
+    	 avg_temp FLOAT,
+    	 precipitation FLOAT,
+    	 snow_fall FLOAT,
+    	 snow_depth FLOAT);
 
 CREATE TABLE sales_2016 
 	(date DATE PRIMARY KEY,
-    transaction_type VARCHAR(10),
-    total_transactions INT,
-    total_sales FLOAT,
-    total_units INT,
-    total_coupons INT,
-    total_return_sales FLOAT,
-    total_return_units INT);
+    	 transaction_type VARCHAR(10),
+    	 total_transactions INT,
+    	 total_sales FLOAT,
+    	 total_units INT,
+    	 total_coupons INT,
+    	 total_return_sales FLOAT,
+    	 total_return_units INT);
     
+CREATE TABLE weather_sales_2016
+	(date DATE PRIMARY KEY,
+	 transaction_type VARCHAR(10),
+	 total_transactions INT,
+	 total_sales FLOAT,
+	 total_units INT,
+	 total_coupons INT,
+	 total_return_sales FLOAT,
+	 total_return_units INT,
+	 max_temp INT,
+	 min_temp FLOAT,
+	 avg_temp FLOAT,
+	 precipitation FLOAT,
+	 snow_fall FLOAT,
+	 snow_depth FLOAT);
