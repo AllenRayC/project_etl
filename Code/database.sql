df679af43af1843467e03463328c01335db81adf
AllenRayC <41596938+AllenRayC@users.noreply.github.com>
2018-11-03 13:05:07 -0400

    Add files via upload

--- Code/database.sql
+++ Code/database.sql
@@ -1,36 +1,37 @@
+DROP DATABASE IF EXISTS weather_data;
 CREATE DATABASE weather_data;
 USE weather_data;
-CREATE TABLE weather_2016 
-	(date DATE PRIMARY KEY,
-    	 max_temp INT,
-    	 min_temp FLOAT,
-    	 avg_temp FLOAT,
-    	 precipitation FLOAT,
-    	 snow_fall FLOAT,
-    	 snow_depth FLOAT);
 
-CREATE TABLE sales_2016 
-	(date DATE PRIMARY KEY,
-    	 transaction_type VARCHAR(10),
-    	 total_transactions INT,
-    	 total_sales FLOAT,
-    	 total_units INT,
-    	 total_coupons INT,
-    	 total_return_sales FLOAT,
-    	 total_return_units INT);
+CREATE TABLE weather_2016 (
+	date DATE PRIMARY KEY,
+	max_temp INT,
+	min_temp INT,
+	avg_temp FLOAT,
+	precipitation FLOAT,
+	snow_fall FLOAT,
+	snow_depth FLOAT
+);
+
+CREATE TABLE sales_2016 (
+	date DATE PRIMARY KEY,
+	transaction_type VARCHAR(10),
+	total_transactions INT,
+	total_sales FLOAT,
+	total_units INT,
+	total_coupons INT
+);
     
-CREATE TABLE weather_sales_2016
-	(date DATE PRIMARY KEY,
-	 transaction_type VARCHAR(10),
-	 total_transactions INT,
-	 total_sales FLOAT,
-	 total_units INT,
-	 total_coupons INT,
-	 total_return_sales FLOAT,
-	 total_return_units INT,
-	 max_temp INT,
-	 min_temp FLOAT,
-	 avg_temp FLOAT,
-	 precipitation FLOAT,
-	 snow_fall FLOAT,
-	 snow_depth FLOAT);
+CREATE TABLE weather_sales_2016 (
+	date DATE PRIMARY KEY,
+	transaction_type VARCHAR(10),
+	total_transactions INT,
+	total_sales FLOAT,
+	total_units INT,
+	total_coupons INT,
+	max_temp INT,
+	min_temp FLOAT,
+	avg_temp FLOAT,
+	precipitation FLOAT,
+	snow_fall FLOAT,
+	snow_depth FLOAT
+);
